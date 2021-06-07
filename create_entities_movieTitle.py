b1c8117e32108829668102dc403161c8d42c9e88
spikeesakshu <spikee.sakshu@gmail.com>
2021-06-07 13:00:21 +0530

    To get only movies :zap:

--- create_entities_movieTitle.py
+++ create_entities_movieTitle.py
@@ -1,24 +1,26 @@
 import json
 import pandas as pd
+import uuid
 
 # movie_entries= json.load(open('data/movieBot/entities/movie_entries_en.json'))
 
 movie_entries= []
-data= pd.read_csv('data/imdb/title_akas.tsv', sep='\t', low_memory=False)
+title_akas= pd.read_csv('data/imdb/title_akas.tsv', sep='\t')
+title_basics= pd.read_csv('data/imdb/title_basics.tsv', sep='\t')
 
-movie_titles= data[(data['language']=='hi') | (data['language']=='en')]
+data= pd.merge(title_akas, title_basics, left_on='titleId', right_on='tconst', right_index= False)
+movie_titles= data[(data['titleType']=='movie') & ((data['language']=='hi') | (data['language']=='en'))]
+print(len(movie_titles.title.unique()))
 
-for title in movie_titles.title.unique():
-    if 'एपिसोड' in title:
-        continue
+for index, title in enumerate(movie_titles.title.unique()):
+
     title_entity= {
         "value": title,
-        # "synonyms": [
-        #     title
-        #     ]
+        "synonyms": [
+            title
+            ]
         }
 
     movie_entries.append(title_entity)
-
 
 json.dump(movie_entries, open('data/movieBot/entities/movie_entries_en.json', 'w'), indent=4 )
