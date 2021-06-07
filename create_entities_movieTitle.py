fbfd382e3541c339bd2692ee679188df2069a0f6
spikeesakshu <spikee.sakshu@gmail.com>
2021-06-07 12:11:11 +0530

    To create movie_entities as required :zap:

--- create_entities_movieTitle.py
+++ create_entities_movieTitle.py
@@ -1,7 +1,24 @@
 import json
 import pandas as pd
 
+# movie_entries= json.load(open('data/movieBot/entities/movie_entries_en.json'))
+
+movie_entries= []
 data= pd.read_csv('data/imdb/title_akas.tsv', sep='\t', low_memory=False)
+
 movie_titles= data[(data['language']=='hi') | (data['language']=='en')]
 
-print(len(movie_titles.title.unique()))
+for title in movie_titles.title.unique():
+    if 'एपिसोड' in title:
+        continue
+    title_entity= {
+        "value": title,
+        # "synonyms": [
+        #     title
+        #     ]
+        }
+
+    movie_entries.append(title_entity)
+
+
+json.dump(movie_entries, open('data/movieBot/entities/movie_entries_en.json', 'w'), indent=4 )
--- get_movie_titles.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-data= pd.read_csv('data/imdb/title_akas.tsv', sep='\t', low_memory=False)
-print(len(data))
-print(data.head())
-print(data.language.unique())
-
-print(data[data['language']=='en'])
